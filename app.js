c2154646b43ee215fe28fec685df0f3783153c95
Olivier Barais <barais@irisa.fr>
2017-12-05 11:22:49 +0100

    first server

---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var formidable = require('formidable');
+var fs = require('fs');
+var tmp = require('tmp');
+var child_process = require('child_process');
+var randomstring = require("randomstring");
+const glob = require('glob');
+var jsel = require('jsel');
+var bodyParser = require('body-parser')
+
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+app.get('/data', function(req, res){
+ 
+    var tab = [];
+
+    for(var i = 0; i < 10;i++){
+      var o = [getRandomInt(0,100),getRandomInt(0,100)];
+      tab.push(o);
+    }      
+    res.json(tab);
+
+});
+  
+app.post('/upload', function(req, res){
+
+});
+
+var server = app.listen(3000, function(){
+  console.log('Server listening on port 3000');
+});
