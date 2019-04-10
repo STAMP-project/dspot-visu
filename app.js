576d9cdce11e320a261a151df7402bd42e375f01
oscarlvp <oscarlvp@gmail.com>
2019-04-10 17:45:19 +0200

    adding project data

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const jsonfile = require('jsonfile');
 
 const app = express();
 app.set('view engine', 'pug')
@@ -13,6 +14,24 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use('/data', express.static(path.join(__dirname, 'data')));
+
+app.get('/slides/:project/', function (request, response) {
+
+  // Early (way too early) functionality
+  const info = jsonfile.readFileSync('projects.json');
+  const projects = info.filter(p => p.name === request.params.project)
+  
+  if(projects.length === 0) {
+    response.status(404);
+    return;
+  }
+
+  response.render('slides.pug', { 
+    project: projects[0],
+    keyboard: request.query.keyboard !== undefined,
+    full_panel: request.query.panel !== undefined
+  });
+});
 
 app.get('/packing/:project/', function(request, response) {
   response.render('packing.pug', {
